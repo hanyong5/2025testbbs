479290e58cdec1e6795f6064b52e60990b254ca3
hanyong5 <hanyong5@naver.com>
2025-10-24 15:30:07 +0900

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import supabase from './utils/supabase';
 
 function App() {
-  return <div>App</div>;
-}
+  const [posts, setPosts] = useState([]);
 
+  useEffect(() => {
+    async function getPosts() {
+      const { data: posts } = await supabase.from('posts').select();
+
+      // if (posts.length > 1) {
+      //   setPosts(posts);
+      // }
+
+      setPosts(posts);
+
+      console.log(posts);
+    }
+
+    getPosts();
+  }, []);
+
+  return (
+    <div>
+      <h3>post</h3>
+      {posts.map((post, i) => (
+        <li key={i}>{post.title}</li>
+      ))}
+    </div>
+  );
+}
 export default App;
