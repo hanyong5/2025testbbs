b30b010168943ac2730de19fdb7b0a5565d14754
hanyong5 <hanyong5@naver.com>
2025-10-28 12:32:01 +0900

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,48 @@
-import { useState, useEffect } from 'react';
-import supabase from './utils/supabase';
-import dayjs from 'dayjs';
+import React from 'react';
+import { BrowserRouter, Link, NavLink, Route, Routes } from 'react-router-dom';
+import './App.css';
+import HomeComp from './pages/home/HomeComp';
+import AboutComp from './pages/about/AboutComp';
+import BoardComp from './pages/board/BoardComp';
 
 function App() {
-  const [posts, setPosts] = useState([]);
-
-  useEffect(() => {
-    async function getPosts() {
-      const { data: posts } = await supabase.from('posts').select();
-
-      console.log(posts);
-      setPosts(posts);
-    }
-
-    getPosts();
-  }, []);
-
   return (
-    <div>
-      <h3>post</h3>
-      <ul>
-        {posts.map((post, i) => (
-          <li key={i}>
-            {post.title} / {post.name} / {post.content} /
-            {dayjs(post.created_at).format('YYYY-MM-DD')}
+    <BrowserRouter>
+      <div className="container d-flex justify-content-between">
+        <h1>
+          <Link to="/" className="nav-link">
+            LOGO
+          </Link>
+        </h1>
+        <ul className="d-flex gap-3 menu">
+          <li className="d-flex align-items-center">
+            {/* <Link to="/" className="nav-link">
+              home
+            </Link> */}
+            <NavLink to="/" className="nav-link">
+              home
+            </NavLink>
           </li>
-        ))}
-      </ul>
-    </div>
+          <li className="d-flex align-items-center">
+            <NavLink to="/about" className="nav-link">
+              about
+            </NavLink>
+          </li>
+          <li className="d-flex align-items-center">
+            <NavLink to="/board" className="nav-link">
+              board
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+      <Routes>
+        <Route path="/" element={<HomeComp />}></Route>
+        <Route path="/about/*" element={<AboutComp />}></Route>
+        <Route path="/board/*" element={<BoardComp />}></Route>
+      </Routes>
+      <div className="container">footer</div>
+    </BrowserRouter>
   );
 }
+
 export default App;
