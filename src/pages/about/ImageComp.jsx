3e9fe885f37243502d33c16ce5ce14c2e6b2547d
hanyong5 <hanyong5@naver.com>
2025-11-13 11:35:58 +0900

    fileupload

--- src/pages/about/ImageComp.jsx
+++ src/pages/about/ImageComp.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { toast } from 'react-toastify';
 import supabase from '../../utils/supabase';
 
@@ -8,12 +8,21 @@ function ImageComp() {
   const [uploadUrl, setUploadUrl] = useState('');
   const [fileName, setFileName] = useState('+');
 
+  // 미리보기 state
+  const [preview, setPreview] = useState('');
+
+  // 미리보기 삭제를 위한 ref  꼭 import
+  const fileInputRef = useRef(null);
+
   const fileChangeHandler = (e) => {
     console.log(e.target.files[0]);
     const file = e.target.files[0];
     setSelectFile(file ?? null);
     setMessage('');
     setFileName(file.name);
+
+    //미리보기
+    setPreview(URL.createObjectURL(file));
   };
 
   const submitHandler = async (e) => {
@@ -24,6 +33,7 @@ function ImageComp() {
     }
 
     const bucket = 'images';
+    const table = 'image_upload';
     const filepath = `${Date.now()}_${selectFile.name}`;
 
     // 파일이름을 uuid +"_" + selectFile.name -> 188912809182..._han.png
@@ -38,14 +48,42 @@ function ImageComp() {
       setMessage('업로드실패 : ' + error.message);
       return;
     } else {
-      toast('업로드 완료되었습니다.');
+      //
+      //
 
       //파일경로전달받음
-      const { data } = supabase.storage.from(bucket).getPublicUrl(filepath);
-      console.log(data.publicUrl);
+      const { data, error: urlErr } = await supabase.storage
+        .from(bucket)
+        .getPublicUrl(filepath);
+      // console.log(data.publicUrl);
       setUploadUrl(data.publicUrl);
-      setFileName('+');
+
+      if (!urlErr) {
+        const { error: insertErr } = await supabase.from(table).insert({
+          filename: fileName,
+          fileurl: uploadUrl,
+        });
+
+        if (!insertErr) {
+          toast('업로드 완료되었습니다.');
+          setFileName('+');
+        }
+      }
+
+      //
+      //
+      //
+      //
+      //
+      //
     }
+  };
+
+  const clearPreview = () => {
+    setPreview('');
+    setSelectFile(null);
+    setFileName('+');
+    fileInputRef.current.value = '';
   };
 
   return (
@@ -72,8 +110,51 @@ function ImageComp() {
                 opacity: 0,
                 top: 0,
               }}
+              ref={fileInputRef}
             />
           </div>
+
+          {preview && (
+            <>
+              <div
+                className="mb-3 position-relative bg-info shadow"
+                style={{
+                  width: '100px',
+                  height: '100px',
+                  borderRadius: '10px',
+                  overflow: 'hidden',
+                }}
+              >
+                <div
+                  className="bg-white  btn-sm position-absolute"
+                  style={{
+                    width: '20px',
+                    height: '20px',
+                    borderRadius: '10px',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    fontSize: '10px',
+                    lineHeight: '10px',
+                    right: '5px',
+                    top: '5px',
+                  }}
+                  onClick={clearPreview}
+                >
+                  X
+                </div>
+                <img
+                  src={preview}
+                  style={{
+                    width: '100px',
+                    height: '100px',
+                    objectFit: 'cover',
+                  }}
+                />
+              </div>
+            </>
+          )}
+
           <button className="btn btn-primary">파일업로드</button>
           <div>{message && <p className="text-danger mt-2">{message}</p>}</div>
           <div>{uploadUrl && <p className=" mt-2">{uploadUrl}</p>}</div>
